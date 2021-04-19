ba8e40d576a1398bb4e69b0315126f272f6ebac9
dock <chris.murdock8648@gmail.com>
2021-04-19 18:32:55 -0400

    modal and hover state stuff

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,9 +1,11 @@
+import "./ProductCategory.scss";
+import "react-image-lightbox/style.css";
+
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import { Base, Col, Flex, Row, Split } from "./layout";
 import Modal from "react-bootstrap/Modal";
-import 'react-image-lightbox/style.css';
-import "./ProductCategory.scss"
+
+import { Base, Col, Flex, Row, Split } from "./layout";
 
 /* Product Catgory */
 
@@ -39,26 +41,33 @@ const ProductItem = ({ product }) => {
 
 
 
-      <Modal show={showModal}>
-        <Base><img  src={images[0]}></img></Base>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{product.description}</Modal.Title>
+        </Modal.Header>
+
+        <Base><img src={images[0]}></img></Base>
       </Modal>
 
 
-      <Base className="image-border-deco" onClick={() => setShowModal(true)}><Carousel interval={null}>
-        {images.map((image) => <Carousel.Item><img src={image}></img></Carousel.Item>)}
-      </Carousel>
-
+      <Base
+        className="image-border-deco"
+        onClick={() => setShowModal(true)}
+        onMouseEnter={() => setShowProductInfo(true)}
+        onMouseLeave={() => setShowProductInfo(false)}
+      >
+        <Carousel interval={null}>
+          {images.map((image) => <Carousel.Item><img src={image}></img></Carousel.Item>)}
+        </Carousel>
       </Base>
 
       {/*Text Section - {description},{dimensions},{price} - need to add an on hover function to display dropdown text otherwise should remain hidded */}
 
-      <Base show={setShowProductInfo} className="product-catagory">
+      <Base className={`${showProductInfo ? "" : " hidden"}`}>
         <Base>{description}</Base>
         <Base>{dimensions}</Base>
         <Base>{price}</Base>
       </Base>
-
-
     </Col>
   );
 };
