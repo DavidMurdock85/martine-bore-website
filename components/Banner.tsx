7830b5f6f28a9e8fcbe6b484b9d7a6fab28e9915
David Murdock <david@MacBook-Pro-2.hitronhub.home>
2022-02-14 15:37:20 -0800

    minor edits

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,13 +6,13 @@ export const Banner: React.FC = () => {
   return (
     <Base className="banner-style">
       <Base className="banner-image-deco">
-        <Image
-          src="images/homepage-images/logo.png"
-          alt="martine bore logo in gold"
-          width="225px"
-          height="205px"
-          className="banner-image"
-        />
+          <Image
+            src="/images/homepage-images/logo.png"
+            alt="martine bore logo in gold"
+            width="225px"
+            height="205px"
+            className="banner-image"
+          />
       </Base>
       <Base tag="h5" className="banner-subtitle">
         Metro Vancouvers Source of Fine Antiques, Jewelry and Art Since 1986
