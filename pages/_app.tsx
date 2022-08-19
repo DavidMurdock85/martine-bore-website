47da223f9ae89c9a26c51eff13c7131a6d109a37
DavidMurdock85 <51424273+DavidMurdock85@users.noreply.github.com>
2022-08-19 11:05:33 -0700

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,36 @@ import "@mb/styles/_app.scss";
 
 import { AuthProvider } from "@mb/providers/AuthProvider";
 import SSRProvider from "react-bootstrap/SSRProvider";
+import Script from "next/script";
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <SSRProvider><AuthProvider><Component {...pageProps} /></AuthProvider></SSRProvider>
+  return (
+    <>
+
+  <Script 
+  strategy='lazyOnload'
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+  />
+
+  <Script
+  id="google-analytics-script"
+  strategy='lazyOnload'>
+  {
+  `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+  `
+  }
+  </Script>
+
+  <SSRProvider><AuthProvider><Component {...pageProps} /></AuthProvider></SSRProvider>
+  </>
+  );
 }
 
 export default MyApp
