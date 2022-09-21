9d8c62baf3a93595ca25dabac64bd641ad5d70fb
dock <chris.murdock8648@gmail.com>
2022-09-21 13:27:49 -0400

    using cols/rows to resize properly

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Base, Flex, Col, Row, Center } from "@mb/components/layout";
 import { Link } from "@mb/components/elements";
+import { Center, Col, Flex, Row } from "@mb/components/layout";
+import React from "react";
 
 export const Services: React.FC = () => {
   return (
@@ -11,15 +11,14 @@ export const Services: React.FC = () => {
         Services
       </Center>
 
-      <Flex
-        flexDirection="row"
+      <Row
         className="services-section-parent"
         ml={1}
         mt={0}
         mr={1}
         mb={1}
        >
-        {/* Sections should resize at different veiws 
+        {/* Sections should resize at different veiws
           . Phone shows 1 section width
           . tablet shows 2 section width
           . desktop shows 4 section width
@@ -27,7 +26,7 @@ export const Services: React.FC = () => {
 
         {/*service section - estates*/}
 
-        <Flex flexDirection="column" className="services-section-child">
+        <Col xs={12} md={6} lg={3} flexDirection="column" className="services-section-child">
           <Flex flexDirection="row" className="services-title" tag="h4">
             Estates
           </Flex>
@@ -39,11 +38,11 @@ export const Services: React.FC = () => {
           <Flex flexDirection="row" className="services-link">
             <Link href="">click for more info</Link>
           </Flex>
-        </Flex>
+        </Col>
 
         {/*service section - appraisals*/}
 
-        <Flex flexDirection="column" className="services-section-child">
+        <Col xs={12} md={6} lg={3} flexDirection="column" className="services-section-child">
           <Flex flexDirection="row" className="services-title" tag="h4">
             Appraisals
           </Flex>
@@ -55,11 +54,11 @@ export const Services: React.FC = () => {
           <Flex flexDirection="row" className="services-link">
             <Link href="">click for more info</Link>
           </Flex>
-        </Flex>
+        </Col>
 
         {/*service section - consignmentss*/}
 
-        <Flex flexDirection="column" className="services-section-child">
+        <Col xs={12} md={6} lg={3} flexDirection="column" className="services-section-child">
           <Flex flexDirection="row" className="services-title" tag="h4">
             Consignments
           </Flex>
@@ -71,11 +70,11 @@ export const Services: React.FC = () => {
           <Flex flexDirection="row" className="services-link">
             <Link href="">click for more info</Link>
           </Flex>
-        </Flex>
+        </Col>
 
         {/*service section - delivery*/}
 
-        <Flex flexDirection="column" className="services-section-child">
+        <Col xs={12} md={6} lg={3} flexDirection="column" className="services-section-child">
           <Flex flexDirection="row" className="services-title" tag="h4">
             Delivery
           </Flex>
@@ -87,8 +86,8 @@ export const Services: React.FC = () => {
           <Flex flexDirection="row" className="services-link">
             <Link href="">click for more info</Link>
           </Flex>
-        </Flex>
-      </Flex>
+        </Col>
+      </Row>
     </Flex>
   );
 };
