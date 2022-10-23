d77d39c154a723bbfb64588fc3fbdbb3c73dbc9a
DavidMurdock85 <51424273+DavidMurdock85@users.noreply.github.com>
2022-10-23 14:59:22 -0700

    updated change

--- components/EverydayAntiques.tsx
+++ components/EverydayAntiques.tsx
@@ -8,7 +8,7 @@ export const EverydayAntiques: React.FC = () => {
       <Row flexDirection="row" xs={12} md={6} lg={6}>
         <Col flexDirection="column">
           <Flex flexDirection="row" justifyContent="center">
-            <Link classname="everyday-antiques-link" href="/categories/seasonal-antiques" m={8}>
+            <Link className="everyday-antiques-link" href="/categories/seasonal-antiques" m={8}>
             <Flex tag="h2"> Everyday Antiques</Flex>
             </Link>
           </Flex>
