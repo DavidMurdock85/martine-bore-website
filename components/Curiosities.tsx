2f0e51a8e86dd5291a5e0f945eab303b09791af5
DavidMurdock85 <51424273+DavidMurdock85@users.noreply.github.com>
2022-10-28 13:45:02 -0700

    updates

--- components/Curiosities.tsx
+++ components/Curiosities.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Flex, Col, Row } from "@mb/components/layout";
 import { Link } from "@mb/components/elements";
 
-export const EverydayAntiques: React.FC = () => {
+export const Curiosities: React.FC = () => {
   return (
-    <Flex flexDirection="column" className="everyday-antiques-parent" tag="a" m={1}>
+    <Flex flexDirection="column" className="curiosities-parent" tag="a" m={1}>
       <Row flexDirection="row" xs={12} md={6} lg={6}>
         <Col flexDirection="column">
           <Flex flexDirection="row" justifyContent="center">
-            <Link className="everyday-antiques-link" href="/categories/seasonal-antiques" m={8}>
-            <Flex tag="h2"> Everyday Antiques</Flex>
+            <Link className="curiosities-link" href="/categories/curiosities" m={8}>
+            <Flex tag="h2">Curiosities</Flex>
             </Link>
           </Flex>
         </Col>
